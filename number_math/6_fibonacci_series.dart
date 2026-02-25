2ee3e37da4e9ebef09e7a2ad2d3c951ad58dde25
Yasir Arafat <yasirarafat6959@gmail.com>
2026-02-26 01:12:00 +0600

    gcd find ps done

--- number_math/6_fibonacci_series.dart
+++ number_math/6_fibonacci_series.dart
@@ -1,15 +1,32 @@
 void main() {
-  fibonacci(10); // প্রথম 10 টা সংখ্যা print করবে
+  // fibonacci(10); // প্রথম 10 টা সংখ্যা print করবে
+
+  int terms = 10;
+  List<int> series = [];
+  for (int i = 0; i < terms; i++) {
+    series.add(fibonacciRecursive(i));
+  }
+  print("Fibonacci series: ${series.join(" ")}");
 }
 
-void fibonacci(int n) {
-  int a = 0, b = 1;
-  print("Fibonacci series up to $n terms:");
-
-  for (int i = 0; i < n; i++) {
-    print(a);
+//
+void fibonacci(int x) {
+  int a = 0;
+  int b = 1;
+  List<int> series = [];
+  for (int i = 0; i < x; i++) {
+    series.add(a);
     int next = a + b;
     a = b;
     b = next;
   }
+  print(series);
+}
+
+
+//using recursion
+int fibonacciRecursive(int n) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
